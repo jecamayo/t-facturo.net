1f03a9ea91364558657252e646d9e5c929e4ea53
jecamayo <vedderman.ten@gmail.com>
2013-09-17 00:43:12 -0500

    prueba

---
+++ SOAPService/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPService.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena (){
+
+            return "Data Source=167874b8-a4ed-412c-b9ed-a23c004c5aea.sqlserver.sequelizer.com; Initial Catalog=db167874b8a4ed412cb9eda23c004c5aea; User ID=ptfvgmmylspfzjov;PWD=33HvUxGaGN7W58gh67NAS6BHmbaZznQDs6wwkQJnmnrJENcBLCp6Pgf5zaiSxQHt";
+            }
+    }
+}
